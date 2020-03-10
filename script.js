f1c46ed41f0ca0ee7bf7ecfdc52685d8e53fe457
Yalmar Mairena <60625019+YalmarMV@users.noreply.github.com>
2020-03-10 17:45:20 -0400

    js file updated

--- script.js
+++ script.js
@@ -1,49 +1,66 @@
 // Assignment Code
-var generateEl = document.querySelector("generate")
-var passwordEl = document.querySelector("password")
-var lengthEl = document.querySelector("length");
-var symbolsEl = document.querySelector("symbols");
-var numbersEl = document.querySelector("numbers");
-var lowerCaseEl = document.querySelector("lowercase");
-var upperCaseEl = document.querySelector("uppercase");
+var generateEl = document.getElementById("generate")
+var passwordEl = document.getElementById("password")
+var lengthEl = document.getElementById("length");
+var symbolsEl = document.getElementById("symbols");
+var numbersEl = document.getElementById("numbers");
+var lowerCaseEl = document.getElementById("lowercase");
+var upperCaseEl = document.getElementById("uppercase");
 
 var randomFunc = {
-  symbol: randomSymbol,
-  number: randomNumber,
-  lower: randomLower,
-  upper: randomUpper,
+  symbol: randomSymbol(),
+  number: randomNumber(),
+  lower: randomLower(),
+  upper: randomUpper(),
 };
 
 // Write password to the #password input
 function generatePassword(symbol, number, lower, upper) {
-  var generatePassword = "";
-  var typesCount = symbol + number + lower + upper;
+  console.log("INSIDE GENERATE FUNCTION")
+  //var generatePassword = "";
+  var typesCount = randomSymbol() + randomNumber() + randomLower() + randomUpper() ;
   var typesArr = [{ symbol }, { number }, { lower }, { upper }].filter(
     item => Object.values(item)[0]
   );
+  console.log(typesCount)
+  var arrsymbol = randomSymbol();
+console.log(arrsymbol)
+  console.log(typesArr)
   if (typesCount === 0) {
     return "";
-  }
 
+  }
+// console.log(randomSymbol())
+// console.log(randomNumber())
+// console.log(randomLower())
+// console.log(randomUpper())
   //Loop
   for (var i = 0; i < length; i += typesCount) {
     typesArr.forEach(type => {
       var funcName = Object.keys(type)[0];
       generatePassword + randomFunc[funcName]();
+    
+      console.log("inside generate password")
     });
+  
   }
 
-  var finalPassword = generatePassword.slice(0, length);
-  return finalPassword;
+  //var finalPassword = generatePassword.slice(0, length);
+  //return finalPassword;
+  return typesCount
 }
+
 
 // Add event listener to generate button
 generateEl.addEventListener("click", function() {
+  console.log("click the buttom")
   var length = +lengthEl.Value;
   var withSymbols = symbolsEl.checked;
   var withNumbers = numbersEl.checked;
   var withlowerCase = lowerCaseEl.checked;
   var withupperCase = upperCaseEl.checked;
+
+// add function for the check boxes all of them and the lenght aswell
 
   passwordEl.innerText = generatePassword(
     withSymbols,
@@ -58,7 +75,7 @@ generateEl.addEventListener("click", function() {
 // Here is my generator functions
 function randomSymbol() {
 
-  var symbols = "!@#$%^&*(){}[]=<>?,.";
+  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "?", ",", "."];
 
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
